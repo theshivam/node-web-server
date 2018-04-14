797cdd5421c8c0c603e9d9be33d022a4a591d3af
Shivam Agarwal <shivam.agarwal18@outlook.com>
2018-04-15 02:24:04 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+
+app.use((req,res,next)=>{
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=> {
+    if (err){
+      console.log('Unable to append to Server Log File !');
+    }
+  });
+  next();
+});
+app.use((req,res,next)=>{
+  res.render('maintenance.hbs');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear',()=>{
+  return (new Date().getFullYear());
+});
+hbs.registerHelper('screamIt',(text)=>{
+  return text.toUpperCase();
+});
+app.get('/',(req,res) => {
+  res.render('home.hbs',{
+    pageTitle:'Home Page',
+    welcomeMessage:'Welcome to the Home Page'
+  });
+});
+app.get('/about',(req,res) => {
+  // res.send('<h1>About Page !');
+  res.render('about.hbs',{
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad',(req,res) => {
+  res.send({
+    errorMessage : 'Unable to handle request'
+  });
+});
+app.listen(3000,()=>{
+  console.log("Server listening at port 3000!");
+});
